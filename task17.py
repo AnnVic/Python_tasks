6c1b2d12a578604b03acf9812bd0b4760f4feb58
Ann <anna@DESKTOP-QAK6MJH.localdomain>
2022-09-23 20:31:09 +0300

    commit

---
+++ task17.py
@@ -0,0 +1,13 @@
+# task №17
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.nytimes.com'
+res = requests.get(url)
+soup = BeautifulSoup(res.text, 'html.parser')
+title = soup.findAll('h3')
+articles = []
+for txt in title:
+    articles += [txt.contents[0]]
+
+print(articles)
