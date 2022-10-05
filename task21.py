546ef6f3d73365df23f691b4f139e5e5b5fe9a2f
Ann <anna@DESKTOP-QAK6MJH.localdomain>
2022-10-05 13:39:29 +0300

    moved the repeating code to the function

--- task21.py
+++ task21.py
@@ -1,22 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+def write_file(name:  str, array: list) -> None:
+    with open(name, 'w') as open_file:
+        for elements in array:
+            open_file.write(elements)
+            open_file.write('\n')
+    return
+
+
 url = 'https://www.nytimes.com'
 res = requests.get(url)
 soup = BeautifulSoup(res.text, 'html.parser')
 title = soup.findAll('h3')
 articles = []
-for txt in title:
-    articles += [txt.contents[0]]
+for text in title:
+    articles.append(text.contents[0])
 
-with open('article.txt', 'w') as open_file:
-    for i in articles:
-        open_file.write(i)
-        open_file.write('\n')
-
-# solution: ask the user to specify the name of the output file
 filename = input('Enter your file name ') + '.txt'
-with open(filename, 'w') as open_file:
-    for i in articles:
-        open_file.write(i)
-        open_file.write('\n')
+
+write_file(filename, articles)
