d170e7055b6dee9e327e9c65b845f71620adbda3
Ann <anna@DESKTOP-QAK6MJH.localdomain>
2022-10-05 13:44:39 +0300

    added the type of data that the functions returns

--- task24.py
+++ task24.py
@@ -1,11 +1,9 @@
-user_answer = int(input('Enter size of board '))
-
-
-def print_board(size: int):
+def print_board(size: int) -> None:
     for i in range(size):
         print(size * ' ---')
         print((size + 1) * '|   ')
     print(size * ' ---')
 
 
+user_answer = int(input('Enter size of board '))
 print_board(user_answer)
