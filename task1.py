336c3cdb984bb5f1504b3fd14dbba1ce91651f8f
Ann <anna@DESKTOP-QAK6MJH.localdomain>
2022-10-05 11:17:53 +0300

    added return to the function

--- task1.py
+++ task1.py
@@ -1,24 +1,21 @@
 # task №1
 import datetime
 
+
+def say_year(name: str, age: int) -> str:
+    current_year = datetime.datetime.now().year
+    return f'{name} in {current_year + (100 - age)} you will be 100 years old.'
+
+
 name = input('Enter your name ')
 age = int(input('Enter your age '))
 number_copy = int(input('Enter number of copy '))
 year = datetime.datetime.now().year + (100 - age)
 
-# solution 1
+
 print(f'{name} in {year} you will be 100 years old.')
 print(f'{name} in {year} you will be 100 years old.\n' * number_copy)
 
-# solution 2
-
-
-def say_year():
-    current_year = datetime.datetime.now().year
-    print(f'{name} in {current_year + (100 - age)} you will be 100 years old.')
-
-
-say_year()
 
 for _ in range(number_copy):
-    say_year()
+    print(say_year(name, age))
