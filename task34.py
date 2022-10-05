30ad8016b7a7f91a34ca565d3ff01811b1f0b6b7
Ann <anna@DESKTOP-QAK6MJH.localdomain>
2022-10-05 14:22:17 +0300

    added the type of data that the function returns

--- task34.py
+++ task34.py
@@ -1,34 +1,34 @@
 import json
 
-info = {}
 
-with open('mydict.json', 'r') as f:
-    info = json.load(f)
+def get_name(answer: str) -> None:
 
-
-def get_name():
-    print('Welcome to the birthday dictionary. We know the birthdays of:')
-    print(*info.keys(), sep='\n')
-
-    user_answer = input('Who\'s birthday do you want to look up? ').title()
-
-    if user_answer in info:
-        print('{} birthday is {}.'.format(user_answer, info[user_answer]))
+    if answer in info:
+        print('{} birthday is {}.'.format(answer, info[answer]))
     else:
-        print('Not found this name {}'.format(user_answer))
+        print('Not found this name {}'.format(answer))
 
 
-def write_new_name(name: str, data: str) -> dict:
+def write_new_name(name: str, data: str) -> None:
     info[name] = data
 
     with open('mydict.json', 'w') as f:
         json.dump(info, f)
 
 
+info = {}
+
+with open('mydict.json', 'r') as f:
+    info = json.load(f)
+
+
 while True:
     answer = input('You want to find name or write a new one? f/w or exit ')
     if answer == 'f':
-        get_name()
+        print('Welcome to the birthday dictionary. We know the birthdays of:')
+        print(*info.keys(), sep='\n')
+        user_answer = input('Who\'s birthday do you want to look up? ').title()
+        get_name(user_answer)
     elif answer == 'w':
         new_name = input('Enter name to add to the dictionary ').title()
         new_data = input('Enter birthday to add to the dictionary ')
