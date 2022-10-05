f0dbbd9bdb73680b9d3197ccd6fd9f75ac6da8d8
Ann <anna@DESKTOP-QAK6MJH.localdomain>
2022-10-05 11:50:08 +0300

    changed variable name

--- task10.py
+++ task10.py
@@ -5,6 +5,6 @@ a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 new_list = [i for i in a and b if i in a and b]
 
-lst = random.sample(range(1, 100), 15)
-lst1 = random.sample(range(1, 100), 20)
-print([i for i in lst and lst1 if i in lst and lst1])
+list1 = random.sample(range(1, 100), 15)
+list2 = random.sample(range(1, 100), 20)
+print([i for i in list1 and list2 if i in list1 and list2])
