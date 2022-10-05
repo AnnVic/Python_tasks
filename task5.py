192685a71cae7d25c54080d2b12b9f543f5017e3
Ann <anna@DESKTOP-QAK6MJH.localdomain>
2022-10-05 11:45:26 +0300

    moved dublicate code to another function

--- task5.py
+++ task5.py
@@ -1,36 +1,36 @@
 # task №5
 import random
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+def get_list(a: list, b: list) -> list:
+    result_list = []
+    for i in a:
+        for j in b:
+            if i == j:
+                result_list.append(i)
+                break
+    return result_list
 
 
 def get_common_number(a: list, b: list) -> list:
-    result_list = []
 
     if len(a) > len(b):
-        for i in a:
-            for j in b:
-                if i == j:
-                    result_list.append(i)
-                    break
+        return get_list(a, b)
     else:
-        for i in b:
-            for j in a:
-                if i == j:
-                    result_list.append(i)
-                    break
-    return result_list
+        return get_list(a, b)
 
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 print(get_common_number(a, b))
 
 # randomly generate two lists
-lst = random.sample(range(1, 50), 10)
-lst1 = random.sample(range(1, 50), 14)
+list1 = random.sample(range(1, 50), 10)
+list2 = random.sample(range(1, 50), 14)
 
-get_common_number(lst, lst1)
+get_common_number(list1, list2)
 
 # solution in one line
 print(list(set(a) & set(b)))
-print(list(set(lst) & set(lst1)))
+print(list(set(list1) & set(list2)))
